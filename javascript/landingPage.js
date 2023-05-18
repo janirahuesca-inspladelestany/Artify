efc88046c9865a651865506d4fdd01aafc4ef6ac
eloybermejo <eloybermejo@inspladelestany.cat>
2023-05-18 14:03:00 +0200

    Canvis finals a la landingpage

--- javascript/landingPage.js
+++ javascript/landingPage.js
@@ -7,12 +7,16 @@ dot.style.left = x + "px";
 dot.style.top = y + "px";
 document.body.appendChild(dot);
 
-document.querySelector(".dot")?.addEventListener("click", function() {
+document.querySelector(".dot").addEventListener("click", function() {
     dot.style.transition = "2s";
     dot.style.transform = "scale(300)";
+
+    setTimeout(function() {
+        window.location.href = "../search.html";
+    }, 1000); 
 });
 
-document.querySelector("span")?.addEventListener("click", function() {
+document.querySelector("span").addEventListener("click", function() {
     const troll_text = document.querySelector("h3");
     if (troll_text === null) {
         return;
